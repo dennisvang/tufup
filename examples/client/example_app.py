f9398d58f964ee2de1f52fb6fc60d6d3ff379ad4
Dennis van Gerwen <29799340+dennisvang@users.noreply.github.com>
2022-04-07 18:23:23 +0200

    add trusted root.json to client example

--- examples/client/example_app.py
+++ examples/client/example_app.py
@@ -1,27 +1,49 @@
 import pathlib
+import shutil
+
 from notsotuf.tools.client import Client
 
+# This example stores all files in the notsotuf/examples/client directory
+BASE_DIR = pathlib.Path(__file__).resolve().parent
 
-METADATA_DIR = pathlib.Path.cwd() / 'metadata'
-TARGET_DIR = pathlib.Path.cwd() / 'target'
+# App info
+APP_NAME = 'example_app'
+CURRENT_VERSION = '1.0'
+# App directories
+CACHE_DIR = BASE_DIR / 'cache'
+METADATA_DIR = CACHE_DIR / 'metadata'
+TARGET_DIR = CACHE_DIR / 'target'
+# Update-server urls
 METADATA_BASE_URL = 'http://localhost:8000/metadata/'
 TARGET_BASE_URL = 'http://localhost:8000/targets/'
 
 
 def main():
-    # ensure dirs exist
+    # The app must ensure dirs exist
     for dir_path in [METADATA_DIR, TARGET_DIR]:
         dir_path.mkdir(exist_ok=True, parents=True)
 
-    # create update client
+    # The app must be shipped with a trusted "root.json" metadata file (
+    # created using tools.repo), and must ensure this file can found in the
+    # specified metadata_dir. The root metadata file lists all trusted keys
+    # and TUF roles.
+    source_path = BASE_DIR / 'trusted_root.json'
+    destination_path = METADATA_DIR / 'root.json'
+    if not destination_path.exists():
+        shutil.copy(src=source_path, dst=destination_path)
+        print('trusted root metadata copied to cache')
+
+    # Create update client
     client = Client(
-        metadata_dir=str(METADATA_DIR),
-        target_dir=str(TARGET_DIR),
+        app_name=APP_NAME,
+        current_version=CURRENT_VERSION,
+        metadata_dir=METADATA_DIR,
         metadata_base_url=METADATA_BASE_URL,
+        target_dir=TARGET_DIR,
         target_base_url=TARGET_BASE_URL,
     )
 
-    # do update
+    # Perform update
     client.update()
 
 
