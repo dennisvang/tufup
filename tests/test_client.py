50f6e9ee68ef387f8dfe070cf234d6282121a02b
Dennis van Gerwen <29799340+dennisvang@users.noreply.github.com>
2022-04-08 22:04:16 +0200

    add rudimentary test for Client.trusted_target_paths

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,5 +1,8 @@
 import gzip
 import pathlib
+from unittest.mock import patch
+
+import tuf.api.exceptions
 
 from notsotuf.client import Client, shutil
 from tests import TempDirTestCase
@@ -7,6 +10,9 @@ from tests import TempDirTestCase
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 TEST_REPO_DIR = BASE_DIR / 'data' / 'repository'
 ROOT_FILENAME = 'root.json'
+TARGETS_FILENAME = 'targets.json'
+SNAPSHOT_FILENAME = 'snapshot.json'
+TIMESTAMP_FILENAME = 'timestamp.json'
 
 
 class UnpackTests(TempDirTestCase):
@@ -45,6 +51,13 @@ class ClientTests(TempDirTestCase):
             target_base_url='http://localhost:8000/targets/',
         )
 
+    def mock_download_metadata(self, rolename: str, *args, **kwargs) -> bytes:
+        if rolename == 'root':
+            # indicate current root is newest version
+            raise tuf.api.exceptions.DownloadHTTPError(status_code=404, message='')
+        file_path = self.metadata_dir / f'{rolename}.json'
+        return file_path.read_bytes()
+
     def test_init_no_metadata(self):
         # cannot initialize without root metadata file
         (self.metadata_dir / ROOT_FILENAME).unlink()
@@ -54,11 +67,23 @@ class ClientTests(TempDirTestCase):
     def test_init(self):
         client = Client(**self.client_kwargs)
         # Client is a subclass of tuf.ngclient.Updater, so it automatically
-        # loads, and verifies, local metadata (if the files are available)
+        # loads and verifies the local root metadata file
         self.assertTrue(client._trusted_set.root)
         # other metadata is not available yet
         for role_name in ['targets', 'snapshot', 'timestamp']:
             self.assertIsNone(getattr(client._trusted_set, role_name))
 
     def test_trusted_target_paths(self):
-        pass
+        # make sure all metadata files are present (these would normally be
+        # downloaded from the update server)
+        for filename in [TARGETS_FILENAME, SNAPSHOT_FILENAME, TIMESTAMP_FILENAME]:
+            shutil.copy(
+                src=TEST_REPO_DIR / 'metadata' / filename,
+                dst=self.metadata_dir / filename,
+            )
+        # refresh to load targets metadata (mock to prevent actual download)
+        client = Client(**self.client_kwargs)
+        with patch.object(client, '_download_metadata', self.mock_download_metadata):
+            client.refresh()
+        # test
+        self.assertTrue(client.trusted_target_paths)
