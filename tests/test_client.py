27fbe157576644491d4e59678c6f067978a329d1
Dennis van Gerwen <29799340+dennisvang@users.noreply.github.com>
2022-04-08 18:28:08 +0200

    add test for Client.__init__, and test data (dummy metadata files and key-pair files)

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,10 +1,13 @@
 import gzip
+import pathlib
 
-from notsotuf.client import shutil
+from notsotuf.client import Client, shutil
 from tests import TempDirTestCase
 
+BASE_DIR = pathlib.Path(__file__).resolve().parent
 
-class UnpackGzipTests(TempDirTestCase):
+
+class UnpackTests(TempDirTestCase):
     def test_unpack_gzip(self):
         # create dummy gzip archive
         archive_path = self.temp_dir_path / 'archive.gz'
@@ -14,3 +17,37 @@ class UnpackGzipTests(TempDirTestCase):
         shutil.unpack_archive(filename=archive_path, extract_dir=self.temp_dir_path)
         expected_file_path = self.temp_dir_path / archive_path.stem
         self.assertTrue(expected_file_path.exists())
+
+
+class ClientTests(TempDirTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.client_kwargs = dict(
+            app_name='test_app',
+            current_version='1.0.0a0',
+            metadata_dir=self.temp_dir_path / 'metadata',
+            metadata_base_url='http://localhost:8000/metadata/',
+            target_dir=self.temp_dir_path / 'targets',
+            target_base_url='http://localhost:8000/targets/',
+        )
+
+    def test_init(self):
+        # client does not create any directories
+        with self.assertRaises(FileNotFoundError):
+            Client(**self.client_kwargs)
+        # directories must be created by parent application
+        for key in ['metadata_dir', 'target_dir']:
+            self.client_kwargs[key].mkdir()
+        # parent application must be shipped with root metadata
+        shutil.copy(
+            src=BASE_DIR / 'data/repository/metadata/root.json',
+            dst=self.client_kwargs['metadata_dir'] / 'root.json',
+        )
+        # now we can initialize the Client
+        client = Client(**self.client_kwargs)
+        # Client is a subclass of tuf.ngclient.Updater, so it automatically
+        # loads, and verifies, local metadata, if available
+        self.assertTrue(client._trusted_set.root)
+        # other metadata is not available yet
+        for role_name in ['targets', 'snapshot', 'timestamp']:
+            self.assertIsNone(getattr(client._trusted_set, role_name))
