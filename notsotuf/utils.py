33a986b5f9331a34d233bba1c3e79a0fe3f9f451
Dennis van Gerwen <29799340+dennisvang@users.noreply.github.com>
2022-04-12 12:12:28 +0200

    tweak utils.start_script_and_exit, and add test

--- notsotuf/utils.py
+++ notsotuf/utils.py
@@ -3,24 +3,37 @@ import pathlib
 import subprocess
 import sys
 from tempfile import NamedTemporaryFile
+from typing import Union
 
 logger = logging.getLogger(__name__)
 
-BAT_TEMPLATE = """@echo off
+MOVE_FILES_BAT = """@echo off
 rem /e: include subdirs, /move: move files and dirs, /v: verbose, /purge: delete stale files and dirs in destination folder
 echo Moving app files...
 robocopy {src} {dst} /e /move /v /purge
 echo Done.
-rem delete self (https://stackoverflow.com/a/20333575)
-(goto) 2>nul & del "%~f0" & pause
+rem Delete self (https://stackoverflow.com/a/20333575)
+(goto) 2>nul & del "%~f0"
 """
 
 
-def start_script_and_exit(src_dir: pathlib.Path, dst_dir: pathlib.Path):
-    script_content = BAT_TEMPLATE.format(src=src_dir, dst=dst_dir)
+def start_script_and_exit(
+        src_dir: Union[pathlib.Path, str], dst_dir: Union[pathlib.Path, str]
+):
+    """
+    Create a batch script that moves files from src to dst, then run the
+    script in a new console, and exit the current process.
+
+    The script is created in a default temporary directory, and deletes
+    itself when done.
+    """
+    script_content = MOVE_FILES_BAT.format(src=src_dir, dst=dst_dir)
     logger.debug(f'writing windows batch script:\n{script_content}')
-    with NamedTemporaryFile(mode='w', prefix='notsotuf', suffix='.bat', delete=False) as temp_file:
+    with NamedTemporaryFile(
+            mode='w', prefix='notsotuf', suffix='.bat', delete=False
+    ) as temp_file:
         temp_file.write(script_content)
+    print(f'Temporary batch script created: {temp_file.name}')
     script_path = pathlib.Path(temp_file.name).resolve()
     logger.debug(f'starting script in new console: {script_path}')
     subprocess.Popen([script_path], creationflags=subprocess.CREATE_NEW_CONSOLE)
---
+++ tests/test_utils.py
@@ -0,0 +1,41 @@
+import subprocess
+from time import sleep
+
+from tests import TempDirTestCase
+
+
+DUMMY_APP_CONTENT = """
+import sys
+from notsotuf.utils import start_script_and_exit
+start_script_and_exit(src_dir=sys.argv[1], dst_dir=sys.argv[2])
+"""
+
+
+class UtilsTests(TempDirTestCase):
+    def test_start_script_and_exit(self):
+        # create src dir with dummy app file, and dst dir with stale subdir
+        test_dir = self.temp_dir_path / 'notsotuf_tests'
+        src_dir = test_dir / 'src'
+        src_dir.mkdir(parents=True)
+        dst_dir = test_dir / 'dst'
+        dst_subdir = dst_dir / 'stale'
+        dst_subdir.mkdir(parents=True)
+        (dst_subdir / 'stale.file').touch()
+        src_file_name = 'dummy_app.py'
+        src_file_path = src_dir / src_file_name
+        src_file_path.write_text(DUMMY_APP_CONTENT)
+        # run the dummy app in a separate process, which, in turn, will run
+        # another process that moves the file
+        completed_process = subprocess.run(
+            ['python', src_file_path, src_dir, dst_dir]
+        )
+        completed_process.check_returncode()
+        # allow some time for the batch file to complete
+        sleep(1)
+        # ensure file has been moved from src to dst
+        self.assertTrue(any(dst_dir.iterdir()))
+        self.assertTrue((dst_dir / src_file_name).exists())
+        # original src file no longer exists
+        self.assertFalse(src_file_path.exists())
+        # stale dst content has been removed (robocopy /purge)
+        self.assertFalse(dst_subdir.exists())
