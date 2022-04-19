0657089a5b4490a68654caba57701adfe900a52d
Dennis van Gerwen <29799340+dennisvang@users.noreply.github.com>
2022-04-19 21:43:21 +0200

    add setup.py, to support editable installs

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+# The setup.py file is only present to support editable installs. In the
+# future we will probably replace setup.py and setup.cfg by pyproject.toml
+#
+# https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html#configuring-setuptools-using-pyproject-toml-files
+setup()
