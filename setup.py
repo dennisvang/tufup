203af2d8570f09cb5d23b83fd49e4ebc7d74cb7a
Dennis van Gerwen <29799340+dennisvang@users.noreply.github.com>
2022-04-19 21:52:45 +0200

    add notsotuf.__version__ and use that in setup.cfg

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 # The setup.py file is only present to support editable installs. In the
 # future we will probably replace setup.py and setup.cfg by pyproject.toml
 #
+# https://github.com/pypa/setuptools/milestone/7
 # https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html#configuring-setuptools-using-pyproject-toml-files
 setup()
---
+++ src/notsotuf/__init__.py
@@ -0,0 +1,2 @@
+# https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
+__version__ = 2022.0
