07a4760608023477e90e3acb036eafa770982705
Dennis van Gerwen <29799340+dennisvang@users.noreply.github.com>
2022-05-27 16:56:05 +0200

    issue9: add __main__.py
    this allows us to call the package directly, e.g. python -m notsotuf ...

---
+++ src/notsotuf/__main__.py
@@ -0,0 +1,10 @@
+import sys
+
+from notsotuf import main
+
+# __main__.py allows us to call the package using `python -m notsotuf`
+# https://docs.python.org/3/library/__main__.html#main-py-in-python-packages
+try:
+    main()  # return code 0
+except Exception as e:
+    sys.exit(f'error: {e}')  # return code 1
