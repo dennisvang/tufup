13cee8ffcfbbb37db5ca4482377477fb6f7f6687
Dennis <29799340+dennisvang@users.noreply.github.com>
2022-06-30 08:55:07 +0200

    Fix issue 17 (#18)
    * issue17:
    add run_bat_as_admin and add as_admin arg to install_update
    
    * issue17:
    pass kwargs from download_and_apply_update into install callable
    
    * issue17:
    add timeout to bat file
    
    * issue17:
    enclose robocopy params in quotes (to deal with spaces in path)
    
    * issue17:
    add debug option to _install_update_win
    
    * issue17:
    move windll import into function, to make it conditional

--- tests/test_utils_platform_specific.py
+++ tests/test_utils_platform_specific.py
@@ -1,10 +1,12 @@
+from getpass import getuser
+import os
 import subprocess
 import sys
 from time import sleep
 import unittest
 
 from notsotuf.utils.platform_specific import (
-    CURRENT_PLATFORM, SUPPORTED_PLATFORMS_FOR_CLIENT
+    ON_WINDOWS, PLATFORM_SUPPORTED, run_bat_as_admin
 )
 from tests import BASE_DIR, TempDirTestCase
 
@@ -12,13 +14,38 @@ DUMMY_APP_CONTENT = f"""
 import sys
 sys.path.append('{(BASE_DIR.parent / 'src').as_posix()}')
 from notsotuf.utils.platform_specific import install_update
-install_update(src_dir=sys.argv[1], dst_dir=sys.argv[2])
+install_update(
+    src_dir=sys.argv[1], dst_dir=sys.argv[2], as_admin=False, debug=False
+)
 """
+
+ON_GITHUB = os.getenv('GITHUB_ACTIONS')
+TEST_RUNAS = os.getenv('TEST_RUNAS')
 
 
 class UtilsTests(TempDirTestCase):
     @unittest.skipIf(
-        condition=CURRENT_PLATFORM not in SUPPORTED_PLATFORMS_FOR_CLIENT,
+        condition=ON_GITHUB or not TEST_RUNAS or not ON_WINDOWS,
+        reason='windows only, requires user interaction',
+    )
+    def test_run_bat_as_admin(self):
+        output_path = self.temp_dir_path / 'output.txt'
+        bat_path = self.temp_dir_path / 'tell_me_who_i_am.bat'
+        bat_path.write_text(f'whoami > "{output_path}"\ntimeout /t -1')
+        # NOTE: this will open a UAC prompt (User Access Control)
+        self.assertTrue(run_bat_as_admin(file_path=bat_path))
+        # doesn't block, so we'll pause for a while
+        sleep(1)
+        self.assertTrue(output_path.exists())
+        output = output_path.read_text()
+        current_user = getuser()
+        print(f'bat file runs as: {output}')
+        print(f'current user: {current_user}')
+        self.assertTrue(len(output))
+        self.assertNotIn(current_user, output)
+
+    @unittest.skipIf(
+        condition=not PLATFORM_SUPPORTED,
         reason='install_update() is only actively supported on windows and mac',
     )
     def test_install_update(self):
@@ -40,8 +67,9 @@ class UtilsTests(TempDirTestCase):
         )
         print(sys.executable)
         completed_process.check_returncode()
-        # allow some time for the batch file to complete
-        sleep(1)
+        # allow some time for the batch file to complete (it also waits a few
+        # seconds, so we have to wait longer)
+        sleep(3)
         # ensure file has been moved from src to dst
         self.assertTrue(any(dst_dir.iterdir()))
         self.assertTrue((dst_dir / src_file_name).exists())
