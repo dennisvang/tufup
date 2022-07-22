bcb66805f405ff41aac338d143cf17db60dff20d
dennisvang <29799340+dennisvang@users.noreply.github.com>
2022-07-22 10:23:15 +0200

    revert fix
    It turns out python-tuf #2047 is not a bug after all, but rather a misconfiguration on the server-side:
    The issue arises if the server sends a `Content-Encoding: gzip` header when it should *not* do so.
    
    fixes #26

--- src/tufup/client.py
+++ src/tufup/client.py
@@ -345,23 +345,7 @@ class AuthRequestsFetcher(RequestsFetcher):
         return session
 
     def _chunks(self, response: "requests.Response") -> Iterator[bytes]:
-        """
-        Override _chunks() to:
-        - prevent automatic decoding of gzip files (python-tuf issue #2047)
-        - call progress hook
-
-        todo: adapt, if necessary, when a fix for python-tuf #2047 is released
-        """
-        try:
-            while True:
-                data = response.raw.read(
-                    amt=self.chunk_size, decode_content=False
-                )
-                if not data:
-                    break
-                self._progress(bytes_new=len(data))
-                yield data
-        except ReadTimeoutError as e:
-            raise SlowRetrievalError from e
-        finally:
-            response.close()
+        """Call progress hook for every chunk."""
+        for data in super()._chunks(response=response):
+            self._progress(bytes_new=len(data))
+            yield data
