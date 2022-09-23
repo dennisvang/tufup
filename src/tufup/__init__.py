0cfa708334e7df6f122d60d67c819ad030bfdb91
dennisvang <29799340+dennisvang@users.noreply.github.com>
2022-09-23 10:40:42 +0200

    master: bump version to 0.4.2

--- src/tufup/__init__.py
+++ src/tufup/__init__.py
@@ -6,7 +6,7 @@ from tufup.utils import input_bool
 
 # https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
 # https://semver.org/
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 
 logger = logging.getLogger(__name__)
 
