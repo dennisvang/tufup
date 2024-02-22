46c13527fd12403fab8346b04e3f38e56af295de
dennisvang <29799340+dennisvang@users.noreply.github.com>
2024-02-22 14:37:26 +0100

    bump version to 0.6.0

--- src/tufup/__init__.py
+++ src/tufup/__init__.py
@@ -5,7 +5,7 @@ from tufup.repo import cli
 
 # https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
 # https://semver.org/
-__version__ = '0.6.0rc'
+__version__ = '0.6.0'
 
 logger = logging.getLogger(__name__)
 
