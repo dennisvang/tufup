4bb16ad228b4b6f39e42527cc4367e501d728142
dennisvang <29799340+dennisvang@users.noreply.github.com>
2024-04-19 11:47:52 +0200

    bump version to 0.8.0
    there's a minor api change in #132 that may affect some users

--- src/tufup/__init__.py
+++ src/tufup/__init__.py
@@ -5,7 +5,7 @@ from tufup.repo import cli
 
 # https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
 # https://semver.org/
-__version__ = '0.7.1'
+__version__ = '0.8.0'
 
 logger = logging.getLogger(__name__)
 
