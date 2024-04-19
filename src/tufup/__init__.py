3e7d99f65a29a0b8895c7c0cd6212a50da49f543
dennisvang <29799340+dennisvang@users.noreply.github.com>
2024-04-19 11:46:25 +0200

    Revert "bump version to 0.7.2"
    This reverts commit c50afe6bfe9f4ab77faf5376e0eb57bc7f3299f9.

--- src/tufup/__init__.py
+++ src/tufup/__init__.py
@@ -5,7 +5,7 @@ from tufup.repo import cli
 
 # https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
 # https://semver.org/
-__version__ = '0.7.2'
+__version__ = '0.7.1'
 
 logger = logging.getLogger(__name__)
 
