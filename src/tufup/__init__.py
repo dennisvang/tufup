5928d4fca035ff6797eec6e9fa537db73f8b003b
dennisvang <29799340+dennisvang@users.noreply.github.com>
2024-06-11 16:01:18 +0200

    set version to 0.8.1

--- src/tufup/__init__.py
+++ src/tufup/__init__.py
@@ -5,7 +5,7 @@ from tufup.repo import cli
 
 # https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
 # https://semver.org/
-__version__ = '0.8.1rc4'
+__version__ = '0.8.1'
 
 logger = logging.getLogger(__name__)
 
