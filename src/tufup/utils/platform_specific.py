c686874b43789a8b3fef6dbbabdf31855ab543ed
Hasan Sezer Taşan <13135006+hasansezertasan@users.noreply.github.com>
2025-08-04 20:52:46 +0300

    fix invalid escape sequence in docstring for install_update  (#172)
    * fix(syntax-warning): resolve invalid escape sequence '\M' own separate directory, such as %PROGRAMFILES%\MyApp.
    
    * fix(docstrings): apply raw docstrings
    
    * revert(escaping): intentional

--- src/tufup/utils/platform_specific.py
+++ src/tufup/utils/platform_specific.py
@@ -24,7 +24,7 @@ def install_update(
     exclude_from_purge: Optional[List[Union[pathlib.Path, str]]] = None,
     **kwargs,
 ):
-    """
+    r"""
     Installs update files using platform specific installation script. The
     actual installation script copies the files and folders from `src_dir` to
     `dst_dir`.
@@ -35,7 +35,7 @@ def install_update(
     **DANGER**:
 
     ONLY use `purge_dst_dir=True` if your app is properly installed in its
-    own *separate* directory, such as %PROGRAMFILES%\MyApp.
+    own *separate* directory, such as `%PROGRAMFILES%\MyApp`.
 
     DO NOT use `purge_dst_dir=True` if your app executable is running
     directly from a folder that also contains unrelated files or folders,
